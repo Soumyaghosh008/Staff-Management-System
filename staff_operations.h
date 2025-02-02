fbbead68205417123a9767e41ebbf754cdcbaa9d
Soumyadeep Ghosh <soumyadeepghosh726@gmail.com>
2025-02-02 13:50:09 +0530

    Fix one bug in the Menu Section

--- staff_operations.h
+++ staff_operations.h
@@ -1,9 +1,9 @@
-#ifndef STUDENT_OPERATIONS_H
-#define STUDENT_OPERATIONS_H
+#ifndef STAFF_OPERATIONS_H
+#define STAFF_OPERATIONS_H
 
 void add_staff_profile();
 void delete_staff_profile();
 void search_staff_profile();
-void show_all__staff_profile();
+void show_all_staff_profile();
 
 #endif
