3968988d4ae5be6b316057ff9a5407e3a1c249fa
Soumyadeep Ghosh <soumyadeepghosh726@gmail.com>
2025-02-03 19:13:54 +0530

    Just adding some basic styling

--- main.c
+++ main.c
@@ -11,12 +11,18 @@ int main(){
 
 // Here we creat the entier manue for the user, to choose the option wisely.
 
+//Just adding some basic styling.
+printf("\t\t\t\t\t #####_____Welcome To Staff Managament System_____#####\n\n\n\n\n");
+
 Try_Again: //Address for the goto feature. 
 
 printf("Enter [1] to Add a Staff Profile \n");
 printf("Enter [2] to Delete a Staff Profile \n");
 printf("Enter [3] to Search a Partcular Staff Profile \n");
 printf("Enter [4] to Show the Complete Dataset of Staff Profiles \n");
+
+// Adding some styling.
+printf("\n\n\n");
 
 // Here we take the option from the user.
      printf("Enter Your Option: ");
@@ -49,7 +55,8 @@ case 1:
 
 // Here we print and compute the try_again feature.
 default:
-printf("404 ERROR. Try Again \n");
+printf("\n\n\n");
+printf("\t\t\t\t\t #####_____404 ERROR.TRY AGAIN_____#####\n\n\n\n\n");;
 
 // Here we compute the calculations for the try again feature. 
 // Note: This feature only works for 3 times only. After that you have to do this again from start.
