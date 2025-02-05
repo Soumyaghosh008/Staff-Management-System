a73e9a145adb2f47dcc67df8a524c93f85a47f7b
Soumyadeep Ghosh <soumyadeepghosh726@gmail.com>
2025-02-05 21:32:17 +0530

    Adding some styling

--- main.c
+++ main.c
@@ -56,8 +56,8 @@ case 1:
 // Here we print and compute the try_again feature.
 default:
 printf("\n\n\n");
-printf("404 ERROR.TRY AGAIN");;
-
+printf("404 ERROR.TRY AGAIN \n");;
+printf("\n\n\n");
 // Here we compute the calculations for the try again feature. 
 // Note: This feature only works for 3 times only. After that you have to do this again from start.
 try++;
