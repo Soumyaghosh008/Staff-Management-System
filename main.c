5f7363f40d8c3a12723ca61034e7de9cf6e94f67
Soumyaghosh008 <soumyadeepghosh726@gmail.com>
2025-02-11 11:57:28 +0530

    Today i completed the debugging & styling. Now all the features working properly.

--- main.c
+++ main.c
@@ -5,80 +5,84 @@
 #include "staff_operations.h"
 // This is the main function which create the options for the user.
 // This function also call each and every user_defined_function as per the choises.
-int main(){
+int main()
+{
 
     int your_option, try = 0;
 
-// Here we creat the entier manue for the user, to choose the option wisely.
+    // Here we creat the entier manue for the user, to choose the option wisely.
 
-//Just adding some basic styling.
+    // Just adding some basic styling.
 
-printf("\n\n"); //Just adding some basic styling.
+    printf("\n\n"); // Just adding some basic styling.
 
-printf("\t\t\t\t\t #####_____Welcome To Staff Managament System_____#####\n\n\n\n\n");
+    printf("\t\t\t\t\t #####_____Welcome To Staff Managament System_____#####\n\n\n\n\n");
 
-Try_Again: //Address for the goto feature. 
+Try_Again: // Address for the goto feature.
 
-printf("Enter [1] to Add a Staff Profile \n");
-printf("Enter [2] to Delete a Staff Profile \n");
-printf("Enter [3] to Search a Partcular Staff Profile \n");
-printf("Enter [4] to Show the Complete Staff Profiles \n");
-printf("Enter [5] to Exit \n");
+    printf("Enter [1] to Add a Staff Profile \n");
+    printf("Enter [2] to Delete a Staff Profile \n");
+    printf("Enter [3] to Search a Partcular Staff Profile \n");
+    printf("Enter [4] to Show the Complete Staff Profiles \n");
+    printf("Enter [5] to Exit \n");
 
-// Adding some styling.
-printf("\n\n\n");
+    // Adding some styling.
+    printf("\n\n\n");
 
-// Here we take the option from the user.
-     printf("Enter Your Option: ");
+    // Here we take the option from the user.
+    printf("Enter Your Option: ");
     scanf("%d", &your_option);
 
-// Here we creat a switch condition to activate a specific operation one at a time.
-// Here we also call the user_defined_functions as per the choises.
-switch (your_option)
-{
+    // Here we creat a switch condition to activate a specific operation one at a time.
+    // Here we also call the user_defined_functions as per the choises.
+    switch (your_option)
+    {
 
-// Here we calling the function which add a staff profile.
-case 1:
-    add_staff_profile();
-    break;
+    // Here we calling the function which add a staff profile.
+    case 1:
+        add_staff_profile();
+        break;
 
-// Here we calling the function which delete a staff profile.
+        // Here we calling the function which delete a staff profile.
     case 2:
-    delete_staff_profile();
-    break;
+        delete_staff_profile();
+        break;
 
-// Here we calling the function which search a specific staff profile.
+        // Here we calling the function which search a specific staff profile.
     case 3:
-    search_staff_profile();
-    break;
+        search_staff_profile();
+        break;
 
-// Here we calling the function which show the complete dataset of staff profiles.
+        // Here we calling the function which show the complete dataset of staff profiles.
     case 4:
-    show_all_staff_profile();
-    break;
+        show_all_staff_profile();
+        break;
 
-// Here we exit from this program.
+        // Here we exit from this program.
     case 5:
-    printf("\n\n\n");
-    printf("\t\t\t\t\t #####_____Goodbye.See You Soon My Friend_____##### \n");;
-    printf("\n\n\n");
-    break;
+        printf("\n\n\n");
+        printf("\t\t\t\t\t #####_____Goodbye.See You Soon My User_____##### \n");
+        ;
+        printf("\n\n\n");
+        break;
 
-// Here we print and compute the try_again feature.
-default:
-printf("\n\n\n");
-printf("404 ERROR.TRY AGAIN \n");;
-printf("\n\n\n");
-// Here we compute the calculations for the try again feature. 
-// Note: This feature only works for 3 times only. After that you have to do this again from start.
-try++;
-if(try < 4){
-    goto Try_Again;
-}else{
-    printf("Do it from Start");
-}
-
-
-}
+    // Here we print and compute the try_again feature.
+    default:
+        printf("\n\n\n");
+        printf("404 ERROR.TRY AGAIN \n");
+        ;
+        printf("\n\n\n");
+        // Here we compute the calculations for the try again feature.
+        // Note: This feature only works for 3 times only. After that you have to do this again from start.
+        try++;
+        if (try < 4)
+        {
+            goto Try_Again;
+        }
+        else
+        {
+            printf("Do it from Start");
+        }
+    }
     return 0;
 }
